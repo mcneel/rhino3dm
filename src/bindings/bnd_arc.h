da628b27495ad044546df72e24240c3cd11e001a
Steve Baer <steve@mcneel.com>
2018-10-20 18:42:10 -0700

    stub out wrappers

--- src/bindings/bnd_arc.h
+++ src/bindings/bnd_arc.h
@@ -11,7 +11,42 @@ void initArcBindings(void* m);
 class BND_Arc : public ON_Arc
 {
 public:
-  BND_Arc(ON_3dPoint a, double b, double c);
+  //public static Arc Unset{ get; }
+  //public Arc(Circle circle, double angleRadians)
+  //public Arc(Circle circle, Interval angleIntervalRadians)
+  //public Arc(Plane plane, double radius, double angleRadians)
+  BND_Arc(ON_3dPoint center, double radius, double angleRadians);
+  //public Arc(Plane plane, Point3d center, double radius, double angleRadians)
+  //public Arc(Point3d startPoint, Point3d pointOnInterior, Point3d endPoint)
+  //public Arc(Point3d pointA, Vector3d tangentA, Point3d pointB)
+  //public bool IsValid{ get; }
+  //public bool IsCircle{ get; }
+  //public Plane Plane{ get; set }
+  //public double Radius{ get; set; }
+  //public double Diameter{ get; set; }
+  //public Point3d Center{ get; set; }
+  //public double Circumference{ get; }
+  //public double Length{ get; }
+  //public Point3d StartPoint{ get; }
+  //public Point3d MidPoint{ get; }
+  //public Point3d EndPoint{ get; }
+  //public Interval AngleDomain{ get; set; }
+  //public double StartAngle{ get; set; }
+  //public double EndAngle{ get; set; }
+  //public double Angle{ get; set; }
+  //public double StartAngleDegrees{ get; set; }
+  //public double EndAngleDegrees{ get; set; }
+  //public double AngleDegrees{ get; set; }
+  //public bool Trim(Interval domain)
+  //public BoundingBox BoundingBox()
+  //public Point3d PointAt(double t)
+  //public Vector3d TangentAt(double t)
+  //public double ClosestParameter(Point3d testPoint)
+  //public Point3d ClosestPoint(Point3d testPoint)
+  //public void Reverse()
+  //public bool Transform(Transform xform)
+  //public NurbsCurve ToNurbsCurve()
+
 
   BND_NurbsCurve* ToNurbsCurve();
 };
--- src/bindings/bnd_arccurve.h
+++ src/bindings/bnd_arccurve.h
@@ -12,8 +12,19 @@ class BND_ArcCurve : public BND_Curve
 {
   ON_ArcCurve* m_arccurve = nullptr;
 public:
-  BND_ArcCurve();
   BND_ArcCurve(ON_ArcCurve* arccurve, const ON_ModelComponentReference* compref);
+  BND_ArcCurve();
+  //public ArcCurve(ArcCurve other)
+  //public ArcCurve(Arc arc)
+  //public ArcCurve(Arc arc, double t0, double t1)
+  //public ArcCurve(Circle circle)
+  //public ArcCurve(Circle circle, double t0, double t1)
+  //public Arc Arc |get;
+  //public bool IsCompleteCircle | get;
+  //public double Radius | get;
+  //public double AngleRadians | get;
+  //public double AngleDegrees | get;
+
 
 protected:
   void SetTrackedPointer(ON_ArcCurve* arccurve, const ON_ModelComponentReference* compref);
