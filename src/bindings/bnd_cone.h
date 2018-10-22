cc75293713903c238a4fa90f137d3179f1b5e4fb
Steve Baer <steve@mcneel.com>
2018-10-22 10:12:42 -0700

    cone bindings

--- src/bindings/bnd_cone.h
+++ src/bindings/bnd_cone.h
@@ -8,22 +8,26 @@ void initConeBindings(pybind11::module& m);
 void initConeBindings(void* m);
 #endif
 
-class BND_Cone : ON_Cone
+class BND_Cone
 {
+public:
+  ON_Cone m_cone;
 public:
   //public Cone(Plane plane, double height, double radius)
   //public Plane Plane{ get; set; }
-  //public double Height{ get; set }
-  //public double Radius{ get; set; }
-  //public bool IsValid{ get; }
-  //public Point3d BasePoint{ get; }
-  //public Point3d ApexPoint{ get; }
-  //public Vector3d Axis{ get; }
-  //public double AngleInRadians()
-  //public double AngleInDegrees()
-  //public NurbsSurface ToNurbsSurface()
-  //public RevSurface ToRevSurface()
-  //public Brep ToBrep(bool capBottom)
+  double GetHeight() const { return m_cone.height; }
+  void SetHeight(double h) { m_cone.height = h; }
+  double GetRadius() const { return m_cone.radius; }
+  void SetRadius(double r) { m_cone.radius = r; }
+  bool IsValid() const { return m_cone.IsValid(); }
+  ON_3dPoint BasePoint() const { return m_cone.BasePoint(); }
+  ON_3dPoint ApexPoint() const { return m_cone.ApexPoint(); }
+  ON_3dPoint Axis() const { return m_cone.Axis(); }
+  double AngleInRadians() const { return m_cone.AngleInRadians(); }
+  double AngleInDegrees() const { return m_cone.AngleInDegrees(); }
+  class BND_NurbsSurface* ToNurbsSurface() const;
+  //class BND_RevSurface* ToRevSurface() const;
+  class BND_Brep* ToBrep(bool capBottom) const;
   //public bool EpsilonEquals(Cone other, double epsilon)
 
 };
