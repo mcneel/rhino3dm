748efc3567d4307dbfc0c6d198ab0acc2bc935bf
Steve Baer <steve@mcneel.com>
2018-10-26 08:56:54 -0700

    encoding/decoding of Sphere for json

--- src/bindings/bnd_sphere.cpp
+++ src/bindings/bnd_sphere.cpp
@@ -1,17 +1,53 @@
 #include "bindings.h"
 
 BND_Sphere::BND_Sphere(ON_3dPoint center, double radius)
-: ON_Sphere(center, radius)
+: m_sphere(center, radius)
 {
 }
 
+void BND_Sphere::SetCenter(const ON_3dPoint& c)
+{
+  m_sphere.plane.origin = c;
+  m_sphere.plane.UpdateEquation();
+}
+
+
 BND_Brep* BND_Sphere::ToBrep()
 {
-  ON_Brep* brep = ON_BrepSphere(*this);
+  ON_Brep* brep = ON_BrepSphere(m_sphere);
   if(brep)
     return new BND_Brep(brep, nullptr);
   return nullptr;
 }
+
+#if defined(ON_PYTHON_COMPILE)
+pybind11::dict BND_Sphere::Encode() const
+{
+  pybind11::dict d;
+  d["Radius"] = m_sphere.radius;
+  pybind11::dict point_dict;
+  point_dict["X"] = m_sphere.Center().x;
+  point_dict["Y"] = m_sphere.Center().y;
+  point_dict["Z"] = m_sphere.Center().z;
+  d["Center"] = point_dict;
+  return d;
+}
+
+BND_Sphere* BND_Sphere::Decode(pybind11::dict jsonObject)
+{
+  double radius = jsonObject["Radius"].cast<double>();
+
+  pybind11::dict center_dict = jsonObject["Center"];
+  ON_3dPoint center;
+  center.x = center_dict["X"].cast<double>();
+  center.y = center_dict["Y"].cast<double>();
+  center.z = center_dict["Z"].cast<double>();
+  return new BND_Sphere(center, radius);
+}
+
+#endif
+
+
 
 #if defined(ON_PYTHON_COMPILE)
 namespace py = pybind11;
@@ -19,9 +55,17 @@ void initSphereBindings(pybind11::module& m)
 {
   py::class_<BND_Sphere>(m, "Sphere")
     .def(py::init<ON_3dPoint, double>())
-    .def_property_readonly("Center", &BND_Sphere::Center)
-    .def_property_readonly("Radius", &BND_Sphere::Radius)
-    .def("ToBrep", &BND_Sphere::ToBrep);
+    .def_property_readonly("IsValid", &BND_Sphere::IsValid)
+    .def_property("Diameter", &BND_Sphere::GetDiameter, &BND_Sphere::SetDiameter)
+    .def_property("Radius", &BND_Sphere::GetRadius, &BND_Sphere::SetRadius)
+    .def_property("Center", &BND_Sphere::GetCenter, &BND_Sphere::SetCenter)
+    .def("PointAt", &BND_Sphere::PointAt)
+    .def("NormalAt", &BND_Sphere::NormalAt)
+    .def("ClosestPoint", &BND_Sphere::ClosestPoint)
+    .def("ToBrep", &BND_Sphere::ToBrep)
+    .def("Encode", &BND_Sphere::Encode)
+    .def_static("Decode", &BND_Sphere::Decode);
+    ;
 }
 #endif
 
--- src/bindings/bnd_sphere.h
+++ src/bindings/bnd_sphere.h
@@ -8,9 +8,53 @@ void initSphereBindings(pybind11::module& m);
 void initSphereBindings(void* m);
 #endif
 
-class BND_Sphere : public ON_Sphere
+class BND_Sphere
 {
 public:
+  ON_Sphere m_sphere;
+public:
   BND_Sphere(ON_3dPoint center, double radius);
+  //public Sphere(Plane equatorialPlane, double radius)
+  //public static Sphere Unset
+  bool IsValid() const { return m_sphere.IsValid(); }
+  //class BND_BoundingBox* GetBoundingBox() const;
+  double GetDiameter() const { return m_sphere.radius * 2; }
+  void SetDiameter(double d) { m_sphere.radius = d * 0.5; }
+  double GetRadius() const { return m_sphere.radius; }
+  void SetRadius(double r) { m_sphere.radius = r; }
+  //public Plane EquatorialPlane | get; set;
+  ON_3dPoint GetCenter() const { return m_sphere.Center(); }
+  void SetCenter(const ON_3dPoint& c);
+  //public Point3d NorthPole
+  //public Point3d SouthPole
+  //public Circle LatitudeRadians(double radians)
+  //public Circle LatitudeDegrees(double degrees)
+  //public Circle LongitudeRadians(double radians)
+  //public Circle LongitudeDegrees(double degrees)
+  ON_3dPoint PointAt(double longitudeRadians, double latitudeRadians) const { return m_sphere.PointAt(longitudeRadians, latitudeRadians); }
+  ON_3dVector NormalAt(double longitudeRadians, double latitudeRadians) const { return m_sphere.NormalAt(longitudeRadians, latitudeRadians); }
+  ON_3dPoint ClosestPoint(const ON_3dPoint& testPoint) const { return m_sphere.ClosestPointTo(testPoint); }
+  //public bool ClosestParameter(Point3d testPoint, out double longitudeRadians, out double latitudeRadians)
+  //public bool Rotate(double sinAngle, double cosAngle, Vector3d axisOfRotation)
+  //public bool Rotate(double angleRadians, Vector3d axisOfRotation)
+  //public bool Rotate(double sinAngle, double cosAngle, Vector3d axisOfRotation, Point3d centerOfRotation)
+  //public bool Rotate(double angleRadians, Vector3d axisOfRotation, Point3d centerOfRotation)
+  //public bool Translate(Vector3d delta)
+  //public bool Transform(Transform xform)
   class BND_Brep* ToBrep();
+  //public NurbsSurface ToNurbsSurface()
+  //public RevSurface ToRevSurface()
+  //public bool EpsilonEquals(Sphere other, double epsilon)
+
+#if defined(__EMSCRIPTEN__)
+  emscripten::val toJSON(emscripten::val key);
+  emscripten::val Encode() const;
+  static BND_Sphere* Decode(emscripten::val jsonObject);
+#endif
+
+#if defined(ON_PYTHON_COMPILE)
+  pybind11::dict Encode() const;
+  static BND_Sphere* Decode(pybind11::dict jsonObject);
+#endif
+
 };
