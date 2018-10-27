67ae2ce63d37a00f75dd2dfc5e1fa9513f269c7f
Steve Baer <steve@mcneel.com>
2018-10-26 22:54:17 -0700

    add material bindings

--- src/bindings/bnd_material.cpp
+++ src/bindings/bnd_material.cpp
@@ -5,6 +5,12 @@ BND_Material::BND_Material()
   SetTrackedPointer(new ON_Material(), nullptr);
 }
 
+BND_Material::BND_Material(const BND_Material& m)
+{
+  ON_Material* material = new ON_Material(*m.m_material);
+  SetTrackedPointer(material, nullptr);
+}
+
 BND_Material::BND_Material(ON_Material* material, const ON_ModelComponentReference* compref)
 {
   SetTrackedPointer(material, compref);
@@ -24,6 +30,26 @@ void initMaterialBindings(pybind11::module& m)
 {
   py::class_<BND_Material, BND_CommonObject>(m, "Material")
     .def(py::init<>())
+    .def(py::init<const BND_Material&>())
+    .def_property("RenderPlugInId", &BND_Material::GetRenderPlugInId, &BND_Material::SetRenderPlugInId)
+    .def_property("Name", &BND_Material::GetName, &BND_Material::SetName)
+    .def_property("Shine", &BND_Material::GetShine, &BND_Material::SetShine)
+    .def_property("Transparency", &BND_Material::GetTransparency, &BND_Material::SetTransparency)
+    .def_property("IndexOfRefraction", &BND_Material::GetIndexOfRefraction, &BND_Material::SetIndexOfRefraction)
+    .def_property("FresnelIndexOfRefraction", &BND_Material::GetFresnelIndexOfRefraction, &BND_Material::SetFresnelIndexOfRefraction)
+    .def_property("RefractionGlossiness", &BND_Material::GetRefractionGlossiness, &BND_Material::SetRefractionGlossiness)
+    .def_property("ReflectionGlossiness", &BND_Material::GetReflectionGlossiness, &BND_Material::SetReflectionGlossiness)
+    .def_property("FresnelReflections", &BND_Material::GetFresnelReflections, &BND_Material::SetFresnelReflections)
+    .def_property("DisableLighting", &BND_Material::GetDisableLighting, &BND_Material::SetDisableLighting)
+    .def_property("Reflectivity", &BND_Material::GetReflectivity, &BND_Material::SetReflectivity)
+    .def_property_readonly("PreviewColor", &BND_Material::GetPreviewColor)
+    .def_property("DiffuseColor", &BND_Material::GetDiffuseColor, &BND_Material::SetDiffuseColor)
+    .def_property("AmbientColor", &BND_Material::GetAmbientColor, &BND_Material::SetAmbientColor)
+    .def_property("EmissionColor", &BND_Material::GetEmissionColor, &BND_Material::SetEmissionColor)
+    .def_property("SpecularColor", &BND_Material::GetSpecularColor, &BND_Material::SetSpecularColor)
+    .def_property("ReflectionColor", &BND_Material::GetReflectionColor, &BND_Material::SetReflectionColor)
+    .def_property("TransparentColor", &BND_Material::GetTransparentColor, &BND_Material::SetTransparentColor)
+    .def("Default", &BND_Material::Default)
     ;
 }
 #endif
--- src/bindings/bnd_material.h
+++ src/bindings/bnd_material.h
@@ -16,5 +16,66 @@ protected:
 
 public:
   BND_Material();
+  BND_Material(const BND_Material& other);
   BND_Material(ON_Material* material, const ON_ModelComponentReference* compref);
+
+  //public void CopyFrom(Material other)
+  BND_UUID GetRenderPlugInId() const { return ON_UUID_to_Binding(m_material->MaterialPlugInId()); }
+  void SetRenderPlugInId(const BND_UUID& id) { m_material->SetMaterialPlugInId(Binding_to_ON_UUID(id)); }
+  int GetMaterialIndex() const { return m_material->Index(); }
+  void SetMaterialIndex(int i) { m_material->SetIndex(i); }
+  std::wstring GetName() const { return std::wstring(m_material->NameAsPointer()); }
+  void SetName(const std::wstring& n) { m_material->SetName(n.c_str()); }
+  //public override ModelComponentType ComponentType
+  static double MaxShine() { return ON_Material::MaxShine; }
+  double GetShine() const { return m_material->Shine(); }
+  void SetShine(double s) { m_material->SetShine(s); }
+  double GetTransparency() const { return m_material->Transparency(); }
+  void SetTransparency(double t) { m_material->SetTransparency(t); }
+  double GetIndexOfRefraction() const { return m_material->m_index_of_refraction; }
+  void SetIndexOfRefraction(double ior) { m_material->m_index_of_refraction = ior; }
+  double GetFresnelIndexOfRefraction() const { return m_material->m_fresnel_index_of_refraction; }
+  void SetFresnelIndexOfRefraction(double ior) { m_material->m_fresnel_index_of_refraction = ior; }
+  double GetRefractionGlossiness() const { return m_material->m_refraction_glossiness; }
+  void SetRefractionGlossiness(double r) { m_material->m_refraction_glossiness = r; }
+  double GetReflectionGlossiness() const { return m_material->m_reflection_glossiness; }
+  void SetReflectionGlossiness(double r) { m_material->m_reflection_glossiness = r; }
+  bool GetFresnelReflections() const { return m_material->FresnelReflections(); }
+  void SetFresnelReflections(bool b) { m_material->SetFresnelReflections(b); }
+  bool GetDisableLighting() const { return m_material->DisableLighting(); }
+  void SetDisableLighting(bool b) { m_material->SetDisableLighting(b); }
+  //public bool AlphaTransparency | get; set;
+  double GetReflectivity() const { return m_material->Reflectivity(); }
+  void SetReflectivity(double r) { m_material->SetReflectivity(r); }
+  BND_Color GetPreviewColor() const { return ON_Color_to_Binding(m_material->PreviewColor()); }
+  BND_Color GetDiffuseColor() const { return ON_Color_to_Binding(m_material->Diffuse()); }
+  void SetDiffuseColor(const BND_Color& c) { m_material->SetDiffuse(Binding_to_ON_Color(c)); }
+  BND_Color GetAmbientColor() const { return ON_Color_to_Binding(m_material->Ambient()); }
+  void SetAmbientColor(const BND_Color& ambient) { m_material->SetAmbient(Binding_to_ON_Color(ambient)); }
+  BND_Color GetEmissionColor() const { return ON_Color_to_Binding(m_material->Emission()); }
+  void SetEmissionColor(const BND_Color& c) { m_material->SetEmission(Binding_to_ON_Color(c)); }
+  BND_Color GetSpecularColor() const { return ON_Color_to_Binding(m_material->Specular()); }
+  void SetSpecularColor(const BND_Color& c) { m_material->SetSpecular(Binding_to_ON_Color(c)); }
+  BND_Color GetReflectionColor() const { return ON_Color_to_Binding(m_material->m_reflection); }
+  void SetReflectionColor(const BND_Color& c) { m_material->m_reflection = Binding_to_ON_Color(c); }
+  BND_Color GetTransparentColor() const { return ON_Color_to_Binding(m_material->m_transparent); }
+  void SetTransparentColor(const BND_Color& c) { m_material->m_transparent = Binding_to_ON_Color(c); }
+  void Default() { *m_material = ON_Material::Default; }
+  //public Texture[] GetTextures()
+  //public Texture GetBitmapTexture()
+  //public bool SetBitmapTexture(string filename)
+  //public bool SetBitmapTexture(Texture texture)
+  //public Texture GetBumpTexture()
+  //public bool SetBumpTexture(string filename)
+  //public bool SetBumpTexture(Texture texture)
+  //public Texture GetEnvironmentTexture()
+  //public bool SetEnvironmentTexture(string filename)
+  //public bool SetEnvironmentTexture(Texture texture)
+  //public Texture GetTransparencyTexture()
+  //public bool SetTransparencyTexture(string filename)
+  //public bool SetTransparencyTexture(Texture texture)
+  //public bool SetUserString(string key, string value)
+  //public string GetUserString(string key)
+  //public int UserStringCount
+  //public System.Collections.Specialized.NameValueCollection GetUserStrings()
 };
