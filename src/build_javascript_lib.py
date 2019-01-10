48217fa221fdeb6a0ee6d672419026c4aca6801a
Will Pearson <will@mcneel.com>
2019-01-10 22:10:43 +0000

    Make rhino3dm.js module name lowercase

--- src/build_javascript_lib.py
+++ src/build_javascript_lib.py
@@ -12,7 +12,7 @@ if not os.path.exists(build_dir):
 
 def compilebinaries():
     os.chdir(build_dir)
-    os.system("emcmake cmake -DCMAKE_CXX_FLAGS=\"-s MODULARIZE=1 -s 'EXPORT_NAME=\\\"Rhino3dm\\\"'\" ../..")
+    os.system("emcmake cmake -DCMAKE_CXX_FLAGS=\"-s MODULARIZE=1 -s 'EXPORT_NAME=\\\"rhino3dm\\\"'\" ../..")
     os.system("make")
     os.system("make install")
     os.chdir("../..")
