f48c066550fdb0f55edca3526105420297651622
Graham Knapp <32717635+dancergraham@users.noreply.github.com>
2019-08-31 00:04:52 +0200

    Update requestboundingbox.py
    Correct typo. Construct loop more succinctly and pythonically

--- samples/python/requestboundingbox.py
+++ samples/python/requestboundingbox.py
@@ -1,9 +1,9 @@
-# Sample to get bounding box of remove 3dm file
+# Sample to get bounding box of remote 3dm file
 from rhino3dm import *
 import requests # pip install requests
 req = requests.get("https://files.mcneel.com/TEST/Rhino Logo.3dm")
 model = File3dm.FromByteArray(req.content)
-for i in range(len(model.Objects)):
-    geometry = model.Objects[i].Geometry
+for obj in model.Objects:
+    geometry = obj.Geometry
     bbox = geometry.GetBoundingBox()
     print("{}, {}".format(bbox.Min, bbox.Max))
