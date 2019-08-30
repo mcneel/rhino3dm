1eb7a1f7a8d008018d8072357269142be921a4db
Graham Knapp <32717635+dancergraham@users.noreply.github.com>
2019-08-31 00:50:48 +0200

    Update __init__.py
    Typo 3D -> 2d

--- src/rhino3dm/__init__.py
+++ src/rhino3dm/__init__.py
@@ -6,7 +6,7 @@ else:
 
 __version__ = '0.5.0'
 
-Point3d.__str__ = lambda self: "{},{}".format(self.X, self.Y)
+Point2d.__str__ = lambda self: "{},{}".format(self.X, self.Y)
 Point3d.__str__ = lambda self: "{},{},{}".format(self.X, self.Y, self.Z)
 Vector2d.__str__ = lambda self: "{},{}".format(self.X, self.Y)
 Vector3d.__str__ = lambda self: "{},{},{}".format(self.X, self.Y, self.Z)
