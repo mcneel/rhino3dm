7cccaf430df6aaeede619b058ac5b2af57f74460
Steve Baer <steve@mcneel.com>
2019-09-03 17:46:20 -0700

    update to 0.7.0

--- setup.py
+++ setup.py
@@ -105,7 +105,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name='rhino3dm',
-    version='0.6.0',
+    version='0.7.0',
     author='Robert McNeel & Associates',
     author_email='steve@mcneel.com',
     description='Python library based on OpenNURBS with a RhinoCommon style',
