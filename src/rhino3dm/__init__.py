264fa9f9bd6ffe018f91efda059bc1d87d2a10f3
Steve Baer <steve@mcneel.com>
2019-12-20 13:15:44 -0800

    Version 0.9.0

--- src/rhino3dm/__init__.py
+++ src/rhino3dm/__init__.py
@@ -4,7 +4,7 @@ if sys.version_info.major==2:
 else:
     from ._rhino3dm import *
 
-__version__ = '0.8.1'
+__version__ = '0.9.0'
 
 Point2d.__str__ = lambda self: "{},{}".format(self.X, self.Y)
 Point3d.__str__ = lambda self: "{},{},{}".format(self.X, self.Y, self.Z)
