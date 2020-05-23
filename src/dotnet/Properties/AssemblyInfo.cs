0f8fe15633681719979b822a0527f025601b1e6b
Will Pearson <will@mcneel.com>
2020-05-23 18:55:00 +0100

    Fix assemblyinfo ifdef

--- src/dotnet/Properties/AssemblyInfo.cs
+++ src/dotnet/Properties/AssemblyInfo.cs
@@ -2,8 +2,8 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
 #if !DOTNETCORE && MOBILE_BUILD
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Rhino3dm")]
