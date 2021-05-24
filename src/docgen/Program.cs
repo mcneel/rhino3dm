fbe68bbc8b64295578c9b1596c36ba81cad6eb9b
Steve Baer <steve@mcneel.com>
2021-05-24 14:16:16 -0700

    Tweak directory search so docgen should work from VS and command line

--- src/docgen/Program.cs
+++ src/docgen/Program.cs
@@ -8,7 +8,7 @@ namespace docgen
         static void Main(string[] args)
         {
             // read all RhinoCommon classes into memory
-            const string rhinocommonPath = @"../dotnet";
+            string rhinocommonPath = SourceDirectory("dotnet");
             Console.WriteLine("[Parse RhinoCommon source]");
             Console.ForegroundColor = ConsoleColor.DarkGreen;
             RhinoCommonClass.BuildClassDictionary(rhinocommonPath);
@@ -16,13 +16,16 @@ namespace docgen
 
             Console.WriteLine("[Parse C++ Bindings]");
             Console.ForegroundColor = ConsoleColor.Green;
-            const string bindingPath = @"../bindings";
+            string bindingPath = SourceDirectory("bindings");
             BindingClass.BuildDictionary(bindingPath);
             Console.ResetColor();
             Console.WriteLine("[END PARSE]");
 
-
-            var outDir = new DirectoryInfo("out");
+            string dir = System.IO.Directory.GetCurrentDirectory();
+            int index = dir.IndexOf("docgen");
+            dir = dir.Substring(0, index + "docgen".Length);
+            string outDirName = Path.Combine(dir, "out");
+            var outDir = new DirectoryInfo(outDirName);
             if (!outDir.Exists)
                 outDir.Create();
 
@@ -36,6 +39,15 @@ namespace docgen
             PythonClass.GenerateApiHelp(Path.Combine(outDir.FullName, "py_apidocs"));
             Console.WriteLine("Writing python type stubs file");
             PythonClass.GenerateTypeStubs(Path.Combine(outDir.FullName, "py_stubs"));
+        }
+
+        static string SourceDirectory(string subdirectoryName)
+        {
+            string dir = System.IO.Directory.GetCurrentDirectory();
+            int index = dir.IndexOf("docgen");
+            dir = dir.Substring(0, index);
+            string path = System.IO.Path.Combine(dir, subdirectoryName);
+            return path;
         }
     }
 }
