b9707ac63bd4b080af71fe5cbfc275e371363ef5
Steve Baer <steve@mcneel.com>
2021-05-27 09:16:27 -0700

    more bindings now that we are using tuples as return types

--- src/bindings/bnd_arc.cpp
+++ src/bindings/bnd_arc.cpp
@@ -30,6 +30,64 @@ void BND_Arc::SetCenter(ON_3dPoint pt)
   m_arc.plane.UpdateEquation();
 }
 
+BND_Interval BND_Arc::AngleDomain() const
+{
+  BND_Interval rc;
+  rc.m_t0 = m_arc.Domain().m_t[0];
+  rc.m_t1 = m_arc.Domain().m_t[1];
+  return rc;
+}
+
+void BND_Arc::SetAngleDomain(BND_Interval interval)
+{
+  m_arc.SetAngleIntervalRadians(ON_Interval(interval.m_t0, interval.m_t1));
+}
+
+double BND_Arc::StartAngle() const
+{
+  return m_arc.Domain().m_t[0];
+}
+
+void BND_Arc::SetStartAngle(double t)
+{
+  ON_Interval domain = m_arc.Domain();
+  domain.m_t[0] = t;
+  m_arc.SetAngleIntervalRadians(domain);
+}
+
+double BND_Arc::EndAngle() const
+{
+  return m_arc.Domain().m_t[1];
+}
+
+void BND_Arc::SetEndAngle(double t)
+{
+  ON_Interval domain = m_arc.Domain();
+  domain.m_t[1] = t;
+  m_arc.SetAngleIntervalRadians(domain);
+}
+
+double BND_Arc::StartAngleDegrees() const
+{
+  return m_arc.DomainDegrees().m_t[0];
+}
+
+void BND_Arc::SetStartAngleDegrees(double t)
+{
+  t = ON_RadiansFromDegrees(t);
+  SetStartAngle(t);
+}
+
+double BND_Arc::EndAngleDegrees() const
+{
+  return m_arc.DomainDegrees().m_t[1];
+}
+
+void BND_Arc::SetEndAngleDegrees(double t)
+{
+  t = ON_RadiansFromDegrees(t);
+  SetEndAngle(t);
+}
 
 bool BND_Arc::Trim(const BND_Interval& domain)
 {
@@ -40,6 +98,13 @@ BND_BoundingBox BND_Arc::BoundingBox() const
 {
   ON_BoundingBox bbox = m_arc.BoundingBox();
   return BND_BoundingBox(bbox);
+}
+
+double BND_Arc::ClosestParameter(ON_3dPoint testPoint) const
+{
+  double t = 0;
+  m_arc.ClosestPointTo(testPoint, &t);
+  return t;
 }
 
 bool BND_Arc::Transform(const BND_Transform& xform)
@@ -77,12 +142,18 @@ void initArcBindings(pybind11::module& m)
     .def_property_readonly("StartPoint", &BND_Arc::StartPoint)
     .def_property_readonly("MidPoint", &BND_Arc::MidPoint)
     .def_property_readonly("EndPoint", &BND_Arc::EndPoint)
+    .def_property("AngleDomain", &BND_Arc::AngleDomain, &BND_Arc::SetAngleDomain)
+    .def_property("StartAngle", &BND_Arc::StartAngle, &BND_Arc::SetStartAngle)
+    .def_property("EndAngle", &BND_Arc::EndAngle, &BND_Arc::SetEndAngle)
     .def_property("AngleRadians", &BND_Arc::GetAngleRadians, &BND_Arc::SetAngleRadians)
+    .def_property("StartAngleDegrees", &BND_Arc::StartAngleDegrees, &BND_Arc::SetStartAngleDegrees)
+    .def_property("EndAngleDegrees", &BND_Arc::EndAngleDegrees, &BND_Arc::SetEndAngleDegrees)
     .def_property("AngleDegrees", &BND_Arc::GetAngleDegrees, &BND_Arc::SetAngleDegrees)
     .def("Trim", &BND_Arc::Trim, py::arg("domain"))
     .def("BoundingBox", &BND_Arc::BoundingBox)
     .def("PointAt", &BND_Arc::PointAt, py::arg("t"))
     .def("TangentAt", &BND_Arc::TangentAt, py::arg("t"))
+    .def("ClosestParameter", &BND_Arc::ClosestParameter, py::arg("testPoint"))
     .def("ClosestPoint", &BND_Arc::ClosestPoint, py::arg("testPoint"))
     .def("Reverse", &BND_Arc::Reverse)
     .def("Transform", &BND_Arc::Transform, py::arg("xform"))
@@ -114,12 +185,18 @@ void initArcBindings(void*)
     .property("startPoint", &BND_Arc::StartPoint)
     .property("midPoint", &BND_Arc::MidPoint)
     .property("endPoint", &BND_Arc::EndPoint)
+    .property("angleDomain", &BND_Arc::AngleDomain, &BND_Arc::SetAngleDomain)
+    .property("startAngle", &BND_Arc::StartAngle, &BND_Arc::SetStartAngle)
+    .property("endAngle", &BND_Arc::EndAngle, &BND_Arc::SetEndAngle)
     .property("angleRadians", &BND_Arc::GetAngleRadians, &BND_Arc::SetAngleRadians)
+    .property("startAngleDegrees", &BND_Arc::StartAngleDegrees, &BND_Arc::SetStartAngleDegrees)
+    .property("endAngleDegrees", &BND_Arc::EndAngleDegrees, &BND_Arc::SetEndAngleDegrees)
     .property("angleDegrees", &BND_Arc::GetAngleDegrees, &BND_Arc::SetAngleDegrees)
     .function("trim", &BND_Arc::Trim)
     .function("boundingBox", &BND_Arc::BoundingBox)
     .function("pointAt", &BND_Arc::PointAt)
     .function("tangentAt", &BND_Arc::TangentAt)
+    .function("closestParameter", &BND_Arc::ClosestParameter)
     .function("closestPoint", &BND_Arc::ClosestPoint)
     .function("reverse", &BND_Arc::Reverse)
     .function("transform", &BND_Arc::Transform)
