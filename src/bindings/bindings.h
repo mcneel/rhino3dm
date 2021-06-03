585362b562d9a9cb924dc492d541db15c402d7ca
Luis Fraguada <fraguada@gmail.com>
2021-06-03 13:24:46 +0200

    any_changed

--- src/bindings/bindings.h
+++ src/bindings/bindings.h
@@ -45,7 +45,7 @@ void SetTuple(BND_TUPLE& tuple, int index, const T& value)
   tuple.set(index, value);
 #endif
 }
-//this is a test 5
+//this is a test 6
 #include "bnd_color.h"
 #include "bnd_file_utilities.h"
 #include "bnd_uuid.h"
