545168c3ef979a86cb387dce5066ac05432d2f02
Luis Fraguada <fraguada@gmail.com>
2021-07-12 12:11:41 +0200

    Add Will's changes for including module.js #406

--- script/build.py
+++ script/build.py
@@ -450,7 +450,7 @@ def build_js():
     run_command("emmake make", True)
 
     # Check to see if the build succeeded and move into artifacts_js
-    items_to_check = ['rhino3dm.wasm', 'rhino3dm.js']
+    items_to_check = ['rhino3dm.wasm', 'rhino3dm.js', 'rhino3dm.module.js']
     all_items_built = True
     for item in items_to_check:
         path_to_item = os.path.abspath(os.path.join(target_path, item))
