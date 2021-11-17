ee664091cfba017af99ae97bb3006c7c50b1634e
Luis Fraguada <fraguada@gmail.com>
2021-11-17 16:06:34 +0100

    Adding TextureWrap

--- src/bindings/bnd_texture.h
+++ src/bindings/bnd_texture.h
@@ -2,6 +2,12 @@
 
 #pragma once
 
+enum class TextureWrap : int
+{
+  Repeat = 0,
+  Clamp = 1,
+};
+
 #if defined(ON_PYTHON_COMPILE)
 void initTextureBindings(pybind11::module& m);
 #else
@@ -24,10 +30,10 @@ public:
   //public TextureType TextureType
   //public int MappingChannelId
   //public TextureCombineMode TextureCombineMode
-  //public TextureUvwWrapping WrapU
-  //public TextureUvwWrapping WrapV
-  //public TextureUvwWrapping WrapW
-  //public Transform UvwTransform
+  TextureWrap WrapU() const { return (TextureWrap) (unsigned int) m_texture->m_wrapu; }
+  TextureWrap WrapV() const { return (TextureWrap) (unsigned int) m_texture->m_wrapv; }
+  TextureWrap WrapW() const { return (TextureWrap) (unsigned int) m_texture->m_wrapw; }
+  BND_Transform UvwTransform() const { return m_texture->m_uvw; }
   //public void GetAlphaBlendValues(out double constant, out double a0, out double a1, out double a2, out double a3)
   //public void SetAlphaBlendValues(double constant, double a0, double a1, double a2, double a3)
   //public void SetRGBBlendValues(Color color, double a0, double a1, double a2, double a3)
