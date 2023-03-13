a5c6669b581df998dc2249617d792030e1399697
fraguada <fraguada@gmail.com>
2023-03-13 11:07:27 +0100

    check if an identity exists for codesigning

--- script/codesign-native.sh
+++ script/codesign-native.sh
@@ -10,6 +10,7 @@ security unlock-keychain -p $MACOS_KEYCHAIN_TEMP_PWD build.keychain
 security import certificate.p12 -k build.keychain -P $MACOS_CERTIFICATE_PWD -T /usr/bin/codesign
 security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k $MACOS_KEYCHAIN_TEMP_PWD build.keychain > /dev/null
 /usr/bin/codesign --force -s $IDENTITY_ID $1 -v
+security find-identity -p codesigning -v
 
 
 ITEMS_TO_SIGN=()
