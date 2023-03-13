3eb065accd3d64e7439ccdea4e39be40c3c90d3e
fraguada <fraguada@gmail.com>
2023-03-13 17:08:27 +0100

    don't setup xcode

--- script/codesign-native.sh
+++ script/codesign-native.sh
@@ -3,14 +3,14 @@
 FOLDER_TO_SEARCH="$1"
 
 # Set up temporary build keychain and insert signing certificate
-echo $MACOS_CERTIFICATE | base64 --decode > certificate.p12
-security create-keychain -p $MACOS_KEYCHAIN_TEMP_PWD build.keychain
-security default-keychain -s build.keychain
-security unlock-keychain -p $MACOS_KEYCHAIN_TEMP_PWD build.keychain
-security import certificate.p12 -k build.keychain -P $MACOS_CERTIFICATE_PWD -T /usr/bin/codesign
-security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k $MACOS_KEYCHAIN_TEMP_PWD build.keychain > /dev/null
-/usr/bin/codesign --force -s $IDENTITY_ID $1 -v
-security find-identity -p codesigning -v
+# echo $MACOS_CERTIFICATE | base64 --decode > certificate.p12
+# security create-keychain -p $MACOS_KEYCHAIN_TEMP_PWD build.keychain
+# security default-keychain -s build.keychain
+# security unlock-keychain -p $MACOS_KEYCHAIN_TEMP_PWD build.keychain
+# security import certificate.p12 -k build.keychain -P $MACOS_CERTIFICATE_PWD -T /usr/bin/codesign
+# security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k $MACOS_KEYCHAIN_TEMP_PWD build.keychain > /dev/null
+# /usr/bin/codesign --force -s $IDENTITY_ID $1 -v
+# security find-identity -p codesigning -v
 
 
 ITEMS_TO_SIGN=()
