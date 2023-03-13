831c90654255342e6af1b02d271594207d4d8797
fraguada <fraguada@gmail.com>
2023-03-13 10:51:18 +0100

    add identity to codesign

--- script/codesign-native.sh
+++ script/codesign-native.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
-# Set up temporary build keychain and insert signing certificate
-# echo $MACOS_CERTIFICATE | base64 --decode > certificate.p12
-# security create-keychain -p $MACOS_KEYCHAIN_TEMP_PWD build.keychain
-# security default-keychain -s build.keychain
-# security unlock-keychain -p $MACOS_KEYCHAIN_TEMP_PWD build.keychain
-# security import certificate.p12 -k build.keychain -P $MACOS_CERTIFICATE_PWD -T /usr/bin/codesign
-# security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k $MACOS_KEYCHAIN_TEMP_PWD build.keychain > /dev/null
-
 FOLDER_TO_SEARCH="$1"
+
+# Set up temporary build keychain and insert signing certificate
+echo $MACOS_CERTIFICATE | base64 --decode > certificate.p12
+security create-keychain -p $MACOS_KEYCHAIN_TEMP_PWD build.keychain
+security default-keychain -s build.keychain
+security unlock-keychain -p $MACOS_KEYCHAIN_TEMP_PWD build.keychain
+security import certificate.p12 -k build.keychain -P $MACOS_CERTIFICATE_PWD -T /usr/bin/codesign
+security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k $MACOS_KEYCHAIN_TEMP_PWD build.keychain > /dev/null
+/usr/bin/codesign --force -s $IDENTITY_ID $1 -v
+
+
 ITEMS_TO_SIGN=()
 if [ -d "$FOLDER_TO_SEARCH" ] ; then
     ITEMS=$(find "${FOLDER_TO_SEARCH}" -depth -type d -name "*.framework" -or -name "*.dylib" -or -name "*.so" -or -name "*.a")
