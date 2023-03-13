26a1083b776be65e2eb66c0a8302c1ff9b68e39d
fraguada <fraguada@gmail.com>
2023-03-13 21:56:22 +0100

    cleanup code-signing

--- script/codesign-native.sh
+++ script/codesign-native.sh
@@ -1,21 +1,15 @@
 #!/usr/bin/env bash
 
-FOLDER_TO_SEARCH="$1"
-
-security default-keychain
-
 # Set up temporary build keychain and insert signing certificate
-# echo $MACOS_CERTIFICATE | base64 --decode > certificate.p12
-#security create-keychain -p $MACOS_KEYCHAIN_TEMP_PWD build.keychain
-#security default-keychain -s build.keychain
+echo $MACOS_CERTIFICATE | base64 --decode > certificate.p12
+security create-keychain -p $MACOS_KEYCHAIN_TEMP_PWD signing_temp.keychain
 security default-keychain -s signing_temp.keychain
-#security unlock-keychain -p $MACOS_KEYCHAIN_TEMP_PWD build.keychain
-# security import certificate.p12 -k build.keychain -P $MACOS_CERTIFICATE_PWD -T /usr/bin/codesign
-#security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k $MACOS_KEYCHAIN_TEMP_PWD build.keychain > /dev/null
-#/usr/bin/codesign --force -s $IDENTITY_ID $1 -v
+security unlock-keychain -p $MACOS_KEYCHAIN_TEMP_PWD signing_temp.keychain
+security import certificate.p12 -k signing_temp.keychain -P $MACOS_CERTIFICATE_PWD -T /usr/bin/codesign
+security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k $MACOS_KEYCHAIN_TEMP_PWD signing_temp.keychain > /dev/null
+/usr/bin/codesign --force -s $IDENTITY_ID $1 -v
 
-security default-keychain
-security find-identity
+FOLDER_TO_SEARCH="$1"
 
 ITEMS_TO_SIGN=()
 if [ -d "$FOLDER_TO_SEARCH" ] ; then
