010da412c4d5235f520fb7a2485b685455fdae1a
fraguada <fraguada@gmail.com>
2023-03-13 17:38:58 +0100

    fix find-identity

--- script/codesign-native.sh
+++ script/codesign-native.sh
@@ -10,7 +10,7 @@ FOLDER_TO_SEARCH="$1"
 # security import certificate.p12 -k build.keychain -P $MACOS_CERTIFICATE_PWD -T /usr/bin/codesign
 # security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k $MACOS_KEYCHAIN_TEMP_PWD build.keychain > /dev/null
 # /usr/bin/codesign --force -s $IDENTITY_ID $1 -v
-security find-identity -p -v
+security find-identity -v
 
 ITEMS_TO_SIGN=()
 if [ -d "$FOLDER_TO_SEARCH" ] ; then
