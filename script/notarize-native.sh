4ddc2fac140dc34a63018ff4b84e408598a9ec34
fraguada <fraguada@gmail.com>
2023-03-20 11:54:52 +0100

    echo path

--- script/notarize-native.sh
+++ script/notarize-native.sh
@@ -1,9 +1,12 @@
 
+
 FILE_TO_NOTARIZE="$1"
 ZIP_PATH=FILE_TO_NOTARIZE + ".zip"
 
 # zip dylib
 /usr/bin/ditto -c -k --keepParent $FILE_TO_NOTARIZE "$ZIP_PATH"
+
+echo "Notarizing '${ZIP_PATH}'"
 
 # notarize - NOTE: --verbose can be added as a switch to get more logging
 /usr/bin/notarytool submit --wait --apple-id "${APPLE_ID}" --password "${MACDEV_PW}" --team-id "${APPLE_TEAM_ID}" $ZIP_PATH --verbose
