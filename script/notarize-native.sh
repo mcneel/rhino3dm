4a5a16fd3eb8879c71732b38e2d9e8aa6add5aa7
fraguada <fraguada@gmail.com>
2023-03-20 11:34:52 +0100

    update arguments

--- script/notarize-native.sh
+++ script/notarize-native.sh
@@ -6,7 +6,7 @@ ZIP_PATH=FILE_TO_NOTARIZE + ".zip"
 /usr/bin/ditto -c -k --keepParent $FILE_TO_NOTARIZE "$ZIP_PATH"
 
 # notarize - NOTE: --verbose can be added as a switch to get more logging
-xcrun notarytool submit --wait --apple-id $APPLE_ID --password $MACDEV_PW --team-id $APPLE_TEAM_ID $ZIP_PATH --verbose
+/usr/bin/notarytool submit --wait --apple-id "${APPLE_ID}" --password "${MACDEV_PW}" --team-id "${APPLE_TEAM_ID}" $ZIP_PATH --verbose
 notarizeStatus=$?
 
 if test $notarizeStatus -ne 0; then
@@ -15,7 +15,7 @@ if test $notarizeStatus -ne 0; then
 fi
 
 # staple for offline validation
-xcrun stapler staple $FILE_TO_NOTARIZE
+/usr/bin/stapler staple "${FILE_TO_NOTARIZE}"
 stapleStatus=$?
 
 if test $stapleStatus -ne 0; then
