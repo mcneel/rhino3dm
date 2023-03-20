20de693263d65ed5ca4ef1613e9c7fbc09cc17a3
fraguada <fraguada@gmail.com>
2023-03-20 12:51:26 +0100

    update notarize script

--- script/notarize-native.sh
+++ script/notarize-native.sh
@@ -1,13 +1,13 @@
 FILE_TO_NOTARIZE="$1"
-ZIP_PATH=FILE_TO_NOTARIZE + ".zip"
+ZIP_PATH="${FILE_TO_NOTARIZE}.zip"
 
 # zip dylib
-/usr/bin/ditto -c -k --keepParent $FILE_TO_NOTARIZE "$ZIP_PATH"
+/usr/bin/ditto -c -k --keepParent "${FILE_TO_NOTARIZE}" "${ZIP_PATH}"
 
-echo "Notarizing '${ZIP_PATH}'"
+echo "Notarizing ${ZIP_PATH}"
 
 # notarize - NOTE: --verbose can be added as a switch to get more logging
-/usr/bin/notarytool submit --wait --apple-id "${APPLE_ID}" --password "${MACDEV_PW}" --team-id "${APPLE_TEAM_ID}" $ZIP_PATH --verbose
+xcrun notarytool submit --wait --apple-id "${APPLE_ID}" --password "${MACDEV_PW}" --team-id "${APPLE_TEAM_ID}" $ZIP_PATH --verbose
 notarizeStatus=$?
 
 if test $notarizeStatus -ne 0; then
