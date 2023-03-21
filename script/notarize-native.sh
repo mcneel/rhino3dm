f526233150552603c4b8247c431be8e15fa7a69e
fraguada <fraguada@gmail.com>
2023-03-21 10:52:27 +0100

    no stapling

--- script/notarize-native.sh
+++ script/notarize-native.sh
@@ -4,10 +4,10 @@ ZIP_PATH="${FILE_TO_NOTARIZE}.zip"
 # zip dylib
 /usr/bin/ditto -c -k --keepParent "${FILE_TO_NOTARIZE}" "${ZIP_PATH}"
 
-echo "Notarizing ${FILE_TO_NOTARIZE}"
+echo "Notarizing ${ZIP_PATH}"
 
 # notarize - NOTE: --verbose can be added as a switch to get more logging
-xcrun notarytool submit --wait --apple-id "${APPLE_ID}" --password "${MACDEV_PW}" --team-id "${APPLE_TEAM_ID}" "${FILE_TO_NOTARIZE}" --verbose
+xcrun notarytool submit --wait --apple-id "${APPLE_ID}" --password "${MACDEV_PW}" --team-id "${APPLE_TEAM_ID}" "${ZIP_PATH}" --verbose
 notarizeStatus=$?
 
 if test $notarizeStatus -ne 0; then
@@ -15,13 +15,13 @@ if test $notarizeStatus -ne 0; then
     exit 1
 fi
 
-chmod -R 755 "${FILE_TO_NOTARIZE}"
+#chmod -R 755 "${FILE_TO_NOTARIZE}"
 
 # staple for offline validation
-xcrun stapler staple "${FILE_TO_NOTARIZE}"
-stapleStatus=$?
+#xcrun stapler staple "${FILE_TO_NOTARIZE}"
+#stapleStatus=$?
 
-if test $stapleStatus -ne 0; then
-    echo "FAILED: failed to staple (see log for details)."
-    exit 1
-fi
+#if test $stapleStatus -ne 0; then
+#    echo "FAILED: failed to staple (see log for details)."
+#    exit 1
+#fi
