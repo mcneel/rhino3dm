59be2b7560d10932931ebf4763051a9e98139934
fraguada <fraguada@gmail.com>
2023-05-01 21:01:04 +0200

    try js nurbssurface points and knots

--- src/bindings/bnd_nurbssurface.cpp
+++ src/bindings/bnd_nurbssurface.cpp
@@ -268,9 +268,9 @@ void initNurbsSurfaceBindings(void*)
     .function("increaseDegreeV", &BND_NurbsSurface::IncreaseDegreeV)
     .property("orderU", &BND_NurbsSurface::OrderU)
     .property("orderV", &BND_NurbsSurface::OrderV)
-    //.property("knotsU", &BND_NurbsSurface::KnotsU)
-    //.property("knotsV", &BND_NurbsSurface::KnotsV)
-    //.property("points", &BND_NurbsSurface::Points)
+    .function("knotsU", &BND_NurbsSurface::KnotsU)
+    .function("knotsV", &BND_NurbsSurface::KnotsV)
+    .function("points", &BND_NurbsSurface::Points)
     ;
 }
 #endif
