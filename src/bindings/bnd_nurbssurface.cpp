6e9b69f442209563fb44ed0dd2cf57035c60d5da
fraguada <fraguada@gmail.com>
2023-05-01 19:30:47 +0200

    try nurbsurface point and knots in js, fix typos

--- src/bindings/bnd_nurbssurface.cpp
+++ src/bindings/bnd_nurbssurface.cpp
@@ -266,11 +266,11 @@ void initNurbsSurfaceBindings(void*)
     .function("makeNonRational", &BND_NurbsSurface::MakeNonRational)
     .function("increaseDegreeU", &BND_NurbsSurface::IncreaseDegreeU)
     .function("increaseDegreeV", &BND_NurbsSurface::IncreaseDegreeV)
-    //.property("orderU", &BND_NurbsSurface::OrderU)
-    //.property("orderV", &BND_NurbsSurface::OrderV)
-    //.property("knotsU", &BND_NurbsSurface::KnotsU)
-    //.property("knotsV", &BND_NurbsSurface::KnotsV)
-    //.property("points", &BND_NurbsSurface::Points)
+    .property("orderU", &BND_NurbsSurface::OrderU)
+    .property("orderV", &BND_NurbsSurface::OrderV)
+    .property("knotsU", &BND_NurbsSurface::KnotsU)
+    .property("knotsV", &BND_NurbsSurface::KnotsV)
+    .property("points", &BND_NurbsSurface::Points)
     ;
 }
 #endif
