be6e1355b947ac511cf4bbdadae85ee149f7c43e
Steve Baer <steve@mcneel.com>
2023-05-07 21:26:32 -0700

    add more c functions for .net

--- src/librhino3dm_native/on_post_effect.cpp
+++ src/librhino3dm_native/on_post_effect.cpp
@@ -1,34 +1,79 @@
 
 #include "stdafx.h"
 
-static const ON_PostEffect* FindPostEffectFromId(ONX_Model& model, const ON_UUID& id)
+ON_3dmRenderSettings& ON_3dmRenderSettings_BeginChange(const ON_3dmRenderSettings* rs);
+const ON_3dmRenderSettings* ON_3dmRenderSettings_FromDocSerial_Internal(unsigned int rhino_doc_sn);
+
+RH_C_FUNCTION const ON_PostEffects* ON_3dmRenderSettings_GetPostEffects(const ON_3dmRenderSettings* rs)
 {
-  //ONX_ModelComponentIterator it(model, ON_ModelComponent::Type::PostEffect);
-  //auto* component = it.FirstComponent();
-  //while (nullptr != component)
-  //{
-  //  const auto* pep = dynamic_cast<const ON_PostEffect*>(component);
-  //  if (nullptr != pep)
-  //  {
-  //    if (pep->Id() == id)
-  //      return pep;
-  //  }
-
-  //  component = it.NextComponent();
-  //}
-
-  return nullptr;
-}
-
-RH_C_FUNCTION const ON_PostEffect* ONX_Model_FindPostEffectFromId(ONX_Model* model, ON_UUID id)
-{
-  if (nullptr == model)
+  if (nullptr == rs)
     return nullptr;
 
-  return FindPostEffectFromId(*model, id);
+  return &rs->PostEffects();
 }
 
-RH_C_FUNCTION int ON_PostEffect_Type(const ON_PostEffect* pep)
+RH_C_FUNCTION ON_PostEffects* ON_3dmRenderSettings_BeginChange_ON_PostEffects(const ON_3dmRenderSettings* rs)
+{
+  return &ON_3dmRenderSettings_BeginChange(rs).PostEffects();
+}
+
+RH_C_FUNCTION const ON_PostEffects* ON_PostEffects_FromDocSerial(unsigned int rhino_doc_sn)
+{
+  const auto* rs = ON_3dmRenderSettings_FromDocSerial_Internal(rhino_doc_sn);
+  if (nullptr == rs)
+    return nullptr;
+
+  return &rs->PostEffects();
+}
+
+RH_C_FUNCTION const ON_PostEffects* ON_PostEffects_FromONX_Model(ONX_Model* ptrModel)
+{
+  if (nullptr == ptrModel)
+    return nullptr;
+
+  return &ptrModel->m_settings.m_RenderSettings.PostEffects();
+}
+
+RH_C_FUNCTION ON_PostEffects* ON_PostEffects_New()
+{
+  return new ON_PostEffects;
+}
+
+RH_C_FUNCTION void ON_PostEffects_Delete(ON_PostEffects* p)
+{
+  delete p;
+}
+
+RH_C_FUNCTION void ON_PostEffects_CopyFrom(ON_PostEffects* target, const ON_PostEffects* source)
+{
+  if ((nullptr != target) && (nullptr != source))
+  {
+    *target = *source;
+  }
+}
+
+RH_C_FUNCTION ON_PostEffect* ON_PostEffects_PostEffectFromId(ON_PostEffects* peps, ON_UUID id)
+{
+  if (nullptr == peps)
+    return nullptr;
+
+  return peps->PostEffectFromId(id);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+RH_C_FUNCTION void ON_PostEffect_GetId(const ON_PostEffect* pep, ON_UUID* id)
+{
+  if ((nullptr != pep) && (nullptr != id))
+  {
+    *id = pep->Id();
+  }
+}
+
+RH_C_FUNCTION unsigned int ON_PostEffect_GetType(const ON_PostEffect* pep)
 {
   if (nullptr == pep)
     return 0;
@@ -36,26 +81,61 @@ RH_C_FUNCTION int ON_PostEffect_Type(const ON_PostEffect* pep)
   return int(pep->Type());
 }
 
-RH_C_FUNCTION void ON_PostEffect_LocalName(const ON_PostEffect* pep, ON_wString* string)
+RH_C_FUNCTION void ON_PostEffect_GetLocalName(const ON_PostEffect* pep, CRhCmnStringHolder* string)
 {
   if ((nullptr != pep) && (nullptr != string))
   {
-    *string = pep->LocalName();
+    string->Set(pep->LocalName());
   }
 }
 
-RH_C_FUNCTION bool ON_PostEffect_Visible(ON_PostEffect* pep)
+RH_C_FUNCTION bool ON_PostEffect_GetOn(const ON_PostEffect* pep)
 {
   if (nullptr == pep)
     return false;
 
-  return false;// pep->IsVisible();
+  return pep->On();
 }
 
-RH_C_FUNCTION bool ON_PostEffect_Active(ON_PostEffect* pep)
+RH_C_FUNCTION void ON_PostEffect_SetOn(ON_PostEffect* pep, bool b)
+{
+  if (nullptr != pep)
+  {
+    pep->SetOn(b);
+  }
+}
+
+RH_C_FUNCTION bool ON_PostEffect_GetShown(const ON_PostEffect* pep)
 {
   if (nullptr == pep)
     return false;
 
-  return false;// pep->IsActive();
+  return pep->Shown();
+}
+
+RH_C_FUNCTION void ON_PostEffect_SetShown(ON_PostEffect* pep, bool b)
+{
+  if (nullptr != pep)
+  {
+    pep->SetShown(b);
+  }
+}
+
+RH_C_FUNCTION unsigned int ON_PostEffect_GetDataCRC(const ON_PostEffect* pep, unsigned int cr)
+{
+  if (nullptr == pep)
+    return 0;
+
+  return pep->DataCRC(cr);
+}
+
+RH_C_FUNCTION const ON_PostEffect* ON_PostEffects_GetAt(const ON_PostEffects* peps, int index)
+{
+  ON_SimpleArray<const ON_PostEffect*> a;
+  peps->GetPostEffects(a);
+
+  if (index >= a.Count())
+    return nullptr;
+
+  return a[index];
 }
