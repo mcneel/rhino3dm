53bd0b259fad0852221f4f8cbeb33f654077c3b3
fraguada <fraguada@gmail.com>
2023-05-18 15:13:08 +0200

    edit convertSpanToBezier for js

--- src/bindings/bnd_nurbscurve.cpp
+++ src/bindings/bnd_nurbscurve.cpp
@@ -318,7 +318,7 @@ void initNurbsCurveBindings(void*)
     .function("reparameterize", &BND_NurbsCurve::Reparameterize)
     .function("grevilleParameter", &BND_NurbsCurve::GrevilleParameter)
     .function("grevillePoint", &BND_NurbsCurve::GrevillePoint)
-    .function("convertSpanToBezier", &BND_NurbsCurve::ConvertSpanToBezier)
+    .function("convertSpanToBezier", &BND_NurbsCurve::ConvertSpanToBezier, allow_raw_pointers())
     .function("points", &BND_NurbsCurve::Points, allow_raw_pointers())
     .function("knots", &BND_NurbsCurve::Knots, allow_raw_pointers())
     ;
