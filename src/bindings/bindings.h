c48f4d05d703f40c35de6486369b711668d05431
fraguada <fraguada@gmail.com>
2023-06-19 23:09:08 +0200

    updating color4f

--- src/bindings/bindings.h
+++ src/bindings/bindings.h
@@ -26,11 +26,13 @@
 #if defined(ON_PYTHON_COMPILE)
 typedef pybind11::dict BND_DICT;
 typedef pybind11::tuple BND_Color;
+typedef pybind11::tuple BND_Color4f;
 typedef pybind11::tuple BND_TUPLE;
 #endif
 #if defined(ON_WASM_COMPILE)
 typedef emscripten::val BND_DICT;
 typedef emscripten::val BND_Color;
+typedef emscripten::val BND_Color4f;
 typedef emscripten::val BND_TUPLE;
 #endif
 
