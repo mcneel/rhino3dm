2f045457cbdc59180d60ca427c56f475bfb3b079
fraguada <fraguada@gmail.com>
2023-06-20 10:58:40 +0200

    don't return void setters

--- src/bindings/bnd_material.h
+++ src/bindings/bnd_material.h
@@ -92,12 +92,12 @@ public:
   //public DocObjects.Texture[] GetTextures()
   bool Supported() const;
   //public void SynchronizeLegacyMaterial()
-  BND_Color4f BaseColor() const { return ON_4fColor_to_Binding(m_material->PhysicallyBased()->BaseColor()); }
-  void SetBaseColor(const BND_Color4f& c) const { return m_material->PhysicallyBased()->SetBaseColor(Binding_to_ON_4fColor(c)); }
-  BND_Color4f EmissionColor() const { return ON_4fColor_to_Binding(m_material->PhysicallyBased()->Emission()); }
-  void SetEmissionColor(const BND_Color4f& c) const { return m_material->PhysicallyBased()->SetEmission(Binding_to_ON_4fColor(c)); }
-  BND_Color4f SubsurfaceScatteringColor() const { return ON_4fColor_to_Binding(m_material->PhysicallyBased()->SubsurfaceScatteringColor()); }
-  void SetSubsurfaceScatteringColor(const BND_Color4f& c) const { return m_material->PhysicallyBased()->SetSubsurfaceScatteringColor(Binding_to_ON_4fColor(c)); }
+  BND_Color4f GetBaseColor() const { return ON_4fColor_to_Binding(m_material->PhysicallyBased()->BaseColor()); }
+  void SetBaseColor(const BND_Color4f& c) const { m_material->PhysicallyBased()->SetBaseColor(Binding_to_ON_4fColor(c)); }
+  BND_Color4f GetEmissionColor() const { return ON_4fColor_to_Binding(m_material->PhysicallyBased()->Emission()); }
+  void SetEmissionColor(const BND_Color4f& c) const { m_material->PhysicallyBased()->SetEmission(Binding_to_ON_4fColor(c)); }
+  BND_Color4f GetSubsurfaceScatteringColor() const { return ON_4fColor_to_Binding(m_material->PhysicallyBased()->SubsurfaceScatteringColor()); }
+  void SetSubsurfaceScatteringColor(const BND_Color4f& c) const { m_material->PhysicallyBased()->SetSubsurfaceScatteringColor(Binding_to_ON_4fColor(c)); }
   //public BRDFs BRDF{ get; set; }
   double Subsurface() const { return m_material->PhysicallyBased()->Subsurface(); }
   void SetSubsurface(double s) { m_material->PhysicallyBased()->SetSubsurface(s); }
