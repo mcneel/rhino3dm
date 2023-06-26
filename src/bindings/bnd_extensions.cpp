f570266e8b61e7e54ad022dbf3ee22c785f70f03
fraguada <fraguada@gmail.com>
2023-06-26 22:08:00 +0200

    cleanup meshmodifier setters

--- src/bindings/bnd_extensions.cpp
+++ src/bindings/bnd_extensions.cpp
@@ -1741,16 +1741,11 @@ void initExtensionsBindings(void*)
     ;
 
   class_<BND_File3dmMeshModifiers>("File3dmMeshModifiers")
-    .property("displacement", &BND_File3dmMeshModifiers::Displacement)
-    .property("edgeSoftening", &BND_File3dmMeshModifiers::EdgeSoftening)
-    .property("thickening", &BND_File3dmMeshModifiers::Thickening)
-    .property("curvePiping", &BND_File3dmMeshModifiers::CurvePiping)
-    .property("shutLining", &BND_File3dmMeshModifiers::ShutLining)
-    .function("createDisplacement", &BND_File3dmMeshModifiers::CreateDisplacement)
-    .function("createEdgeSoftening", &BND_File3dmMeshModifiers::CreateEdgeSoftening)
-    .function("createThickening ", &BND_File3dmMeshModifiers::CreateThickening)
-    .function("createCurvePiping", &BND_File3dmMeshModifiers::CreateCurvePiping)
-    .function("createShutLining ", &BND_File3dmMeshModifiers::CreateShutLining)
+    .property("displacement", &BND_File3dmMeshModifiers::Displacement, &BND_File3dmMeshModifiers::CreateDisplacement)
+    .property("edgeSoftening", &BND_File3dmMeshModifiers::EdgeSoftening, &BND_File3dmMeshModifiers::CreateEdgeSoftening)
+    .property("thickening", &BND_File3dmMeshModifiers::Thickening, &BND_File3dmMeshModifiers::CreateThickening)
+    .property("curvePiping", &BND_File3dmMeshModifiers::CurvePiping, &BND_File3dmMeshModifiers::CreateCurvePiping)
+    .property("shutLining", &BND_File3dmMeshModifiers::ShutLining, &BND_File3dmMeshModifiers::CreateShutLining)
     ;
 
   class_<BND_File3dmShutLiningCurveTable>("File3dmShutLiningCurveTable")
