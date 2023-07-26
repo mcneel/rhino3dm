4e87681f2ddd3002899a2ab9325ec0e7d83b3a0e
croudyj <croudyj@gmail.com>
2023-07-25 18:05:03 -0700

    Experimental changes to BND_File3dmGroundPlane::BND_File3dmGroundPlane(const BND_File3dmGroundPlane& gp)

--- src/bindings/bnd_ground_plane.cpp
+++ src/bindings/bnd_ground_plane.cpp
@@ -9,8 +9,28 @@ BND_File3dmGroundPlane::BND_File3dmGroundPlane()
 
 BND_File3dmGroundPlane::BND_File3dmGroundPlane(const BND_File3dmGroundPlane& gp)
 {
-  _gp = new ON_GroundPlane(*gp._gp);
+  // Luis: I failed to get shared pointers to work because making the shared pointer
+  // causes a copy of the ground plane to be created, which results in changes not sticking.
+  // It seems that to make it work with shared pointers I would have to change the OpenNURBS
+  // implementation to also use shared pointers. I can talk to Andy about this soon.
+
+  // For now I decided to abandon that idea and see if I could work around the fact that JS calls
+  // this copy constructor. The following is an experiment which I can't test because Python doesn't
+  // call it. I 'steal' the incoming ground plane's native object into this one and assume JS is going
+  // to forget about the incoming one. I'd be interested to know if this fixes the problem.
+
+  // Steal the pointer to the native object.
+  _gp = gp._gp;
+
+  // Tell the original owner that it no longer owns it.
+  const_cast<BND_File3dmGroundPlane&>(gp)._owned = false;
+
+  // This object now owns it.
   _owned = true;
+
+// Old code makes an actual copy of the native object -- which means changes don't stick.
+//  _gp = new ON_GroundPlane(*gp._gp);
+//  _owned = true;
 }
 
 BND_File3dmGroundPlane::BND_File3dmGroundPlane(ON_GroundPlane* gp)
