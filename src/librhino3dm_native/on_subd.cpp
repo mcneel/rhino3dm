01875f11ce4f5c58fa22128e00d337bd48522c18
fraguada <fraguada@gmail.com>
2023-11-16 15:30:43 +0100

    reverting opennurbs plus leak

--- src/librhino3dm_native/on_subd.cpp
+++ src/librhino3dm_native/on_subd.cpp
@@ -1,7 +1,6 @@
 #include "stdafx.h"
 
 RH_C_SHARED_ENUM_PARSE_FILE("../../../opennurbs/opennurbs_subd.h")
-RH_C_SHARED_ENUM_PARSE_FILE("../../../opennurbs/opennurbs_plus_subd.h")
 
 
 RH_C_FUNCTION ON_SubDRef* ON_SubDRef_New()
