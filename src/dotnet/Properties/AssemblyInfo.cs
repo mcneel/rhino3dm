2aea235a3a2a2a608d034f4d88f774b8d5d5440b
fraguada <fraguada@gmail.com>
2023-11-17 10:46:12 +0100

    remove docgen from js workflow.
    the typescript defs should be combed over by a human (for now)

--- src/dotnet/Properties/AssemblyInfo.cs
+++ src/dotnet/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Robert McNeel & Associates")]
 [assembly: AssemblyProduct("Rhino")]
-[assembly: AssemblyCopyright("Copyright © 2022")]
+[assembly: AssemblyCopyright("Copyright © 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 // Brian Gillespie: 9/22/2010
