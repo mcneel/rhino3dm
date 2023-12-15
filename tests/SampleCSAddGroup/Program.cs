a9439e8cc5f520426f0ae1f4652e508a9613b1ae
fraguada <fraguada@gmail.com>
2023-12-15 22:28:37 +0100

    simple console program for debug

---
+++ tests/SampleCSAddGroup/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+var doc = new Rhino.FileIO.File3dm();
+var index = doc.AllGroups.AddGroup();
+Console.WriteLine("nº of groups {0}",doc.AllGroups.Count);
+
+for( int i = 1; i < 10; i ++ ) {
+   var circle = new Rhino.Geometry.Circle(i);
+   var id = doc.Objects.AddCircle(circle);
+   var ro = doc.Objects.FindId(id);
+   ro.Attributes.AddToGroup(index);
+   Console.WriteLine(ro.Attributes.GroupCount);
+}
+
+var tmpPath = System.IO.Path.GetTempPath();
+tmpPath = System.IO.Path.Combine(tmpPath, "testGroup.3dm");
+
+doc.Write(tmpPath, 8);
+Console.WriteLine(tmpPath);
+
