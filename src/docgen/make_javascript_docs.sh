b8a2f43f4d6f62b283cc4266e9be4fba30c26853
fraguada <fraguada@gmail.com>
2024-02-16 13:09:16 +0100

    add new js docs

--- src/docgen/make_javascript_docs.sh
+++ src/docgen/make_javascript_docs.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #assumes you've installed jsdoc and docdash via npm i -g docdash jsdoc
+#might need to run it from npm global folder:  ~/.npm-global/bin/jsdoc
 jsdoc ./out/js_apidocs/rh3dm_temp.js README.md -c jsdoc.conf -t ~/.npm-global/lib/node_modules/docdash -d ../../docs/javascript/api
