2aff4e406c8a355b67d0da63a272a0cab091465e
fraguada <fraguada@gmail.com>
2024-02-20 12:36:59 +0100

    add Read Text Entity Sample to test on Linux

---
+++ tests/SampleCSReadTextEntity/Program.cs
@@ -0,0 +1,33 @@
+﻿using Rhino.FileIO;
+
+Console.WriteLine("---Sample: Read Text Entities from file---");
+
+var path = Directory.GetCurrentDirectory();
+
+Console.WriteLine("Current Directory: {0}",path);
+
+File3dm file3dm = new File3dm();
+
+//the path will be different whether you are using dotnet run or debugging
+if(path.Contains("net7.0")){
+    file3dm = File3dm.Read("../../../textEntities_r8.3dm");
+} else{
+    file3dm = File3dm.Read("textEntities_r8.3dm");
+} 
+
+Console.WriteLine("Number of objects in file {0}", file3dm.Objects.Count);
+
+foreach( var ro in file3dm.Objects )
+{
+    var te = ro.Geometry as Rhino.Geometry.TextEntity;
+    if( te != null) {
+        string plainText = te.PlainText;
+        Console.WriteLine("-----------");
+        Console.WriteLine("Plain text: {0}", plainText);
+        Console.WriteLine("Rich text: {0}", te.RichText);
+        Console.WriteLine("Plain text with fields: {0}", te.PlainTextWithFields);
+    }
+}
+
+var sw = new Rhino.Runtime.InteropWrappers.StringWrapper("hi");
+Console.WriteLine("StringWrapper: {0}", sw.ToString());
