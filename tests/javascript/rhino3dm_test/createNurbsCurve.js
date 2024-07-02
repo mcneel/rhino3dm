c90411b4a4cd7e29d3188ff0d22921318eb7143b
Luis E. Fraguada <luis@mcneel.com>
2024-07-02 16:53:43 +0000

    fix curve test

--- tests/javascript/rhino3dm_test/createNurbsCurve.js
+++ tests/javascript/rhino3dm_test/createNurbsCurve.js
@@ -7,11 +7,11 @@ async function createNurbsCurve() {
     const pointArray = [ [ 0, 0, 0 ], [ 1, 1, 0 ], [ 2, 0, 0 ], [ 3, -1, 0 ], [ 4, 0, 0 ] ]
 
     const pointList = new rhino.Point3dList(5)
-    points.add(0, 0, 0)
-    points.add(1, 1, 0)
-    points.add(2, 0, 0)
-    points.add(3, -1, 0)
-    points.add(4, 0, 0)
+    pointList.add(0, 0, 0)
+    pointList.add(1, 1, 0)
+    pointList.add(2, 0, 0)
+    pointList.add(3, -1, 0)
+    pointList.add(4, 0, 0)
 
     const crvFromArray = rhino3dm.NurbsCurve.create(false, 3, pointArray)
     const crvFromList = rhino3dm.NurbsCurve.create(false, 3, pointList)
