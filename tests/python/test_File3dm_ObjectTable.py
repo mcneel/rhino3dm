8f266d1284ae2cd0a3b001dcb40322edd3983234
StudioWEngineers <studio.w.engineers@gmail.com>
2024-10-23 14:15:38 +0100

    add tests

--- tests/python/test_File3dm_ObjectTable.py
+++ tests/python/test_File3dm_ObjectTable.py
@@ -24,6 +24,27 @@ class TestFile3dmObjectTable(unittest.TestCase):
 
         self.assertTrue(objqty == 2 and isCurve1 and isCurve2 and len1 == 15 and len2 == 15)
 
+
+    def test_negativeIndexing(self) -> None:
+        """Tests for indexing `ObjectTable`.
+        """
+        file_3dm = rhino3dm.File3dm()
+        for i in range(2):
+            file_3dm.Objects.AddPoint(rhino3dm.Point3d(i, i, i))
+
+        with self.assertRaises(IndexError, msg="Test negative IndexError"):
+            file_3dm.Objects[-3]
+
+        with self.assertRaises(IndexError, msg="Test positive IndexError"):
+            file_3dm.Objects[3]
+
+        with self.subTest(msg="Test positive indexing"):
+            self.assertEqual(file_3dm.Objects[1].Geometry.Location, rhino3dm.Point3d(1, 1, 1))
+
+        with self.subTest(msg="Test negative indexing"):
+            self.assertEqual(file_3dm.Objects[-2].Geometry.Location, rhino3dm.Point3d(0, 0, 0))
+
+
 if __name__ == '__main__':
     print("running tests")
     unittest.main()
